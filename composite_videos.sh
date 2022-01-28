7e44b85c8556e9ef5d01497c6fd93063b2678431
Max Siegel <maxs@mit.edu>
2022-01-28 09:56:49 -0500

    composite script

--- composite_videos.sh
+++ composite_videos.sh
@@ -1,3 +1,14 @@
+TWICE_LEN=10
 
-ffmpeg -i videos/boxA_camA_liquidA.mp4 -filter_complex 'pad=1120:420:280:0' videos/padded.mp4
-ffmpeg -i videos/padded.mp4 -i videos/boxA_camA_liquidA.mp4 -i videos/boxB_camB_liquidB.mp4 -filter_complex "[1]tpad=start_mode=clone:start_duration=5[tr];[1][2]hstack=inputs=2[p2];[0][p2]vstack=inputs=2" videos/composite.mp4
+ffmpeg -i videos/boxA_camA_liquidA.mp4 -filter_complex 'pad=1120:420:280:0' videos/horizontal_padded.mp4
+ffmpeg -i videos/horizontal_padded.mp4 -filter_complex 'tpad=stop_duration=10' videos/horizontal_and_time_padded.mp4
+
+ffmpeg -i videos/boxB_camB_liquidA.mp4 -filter_complex 'tpad=start_duration=5' videos/time_padded_temp_A.mp4
+ffmpeg -i videos/time_padded_temp_A.mp4 -filter_complex 'tpad=stop_duration=5' videos/time_padded_A.mp4
+
+# ffmpeg -i videos/boxB_camB_liquidB.mp4 -filter_complex 'tpad=start_duration=\5' videos/time_padded_temp_B.mp4
+ffmpeg -i videos/boxB_camB_liquidB.mp4 -filter_complex 'tpad=start_duration=10' videos/time_padded_B.mp4
+
+rm videos/time_padded_temp_*
+
+ffmpeg -y -i videos/horizontal_and_time_padded.mp4 -i videos/time_padded_A.mp4 -i videos/time_padded_B.mp4 -filter_complex "[1][2]hstack=inputs=2[p2];[0][p2]vstack=inputs=2" videos/composite.mp4
