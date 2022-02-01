545afbd639198951427c62fc7275eea7ef5ec899
Max Siegel <maxs@mit.edu>
2022-02-01 14:26:17 -0500

    _

--- add_black_end_frame.sh
+++ add_black_end_frame.sh
@@ -1,13 +1,19 @@
-for i in $(seq 1 3)
-do
+# for i in $(seq 1 3)
+# do
     # for f in stimuli/Group$i/material/*
     # do
     #     ffmpeg -hide_banner  -y -i $f -filter_complex 'tpad=stop=1' stimuli/Group$i/shape/$(basename $f .mp4)_fixed.mp4
     #     mv stimuli/Group$i/shape/$(basename $f .mp4)_fixed.mp4 stimuli/Group$i/shape/$(basename $f .mp4).mp4
     # done
-    for f in stimuli/Group$i/shape/*
-    do
-        # ffmpeg -hide_banner  -y -i $f -filter_complex 'tpad=stop=1' stimuli/Group$i/material/$(basename $f .mp4)_fixed.mp4
-        mv stimuli/Group$i/material/$(basename $f .mp4)_fixed.mp4 stimuli/Group$i/material/$(basename $f .mp4).mp4
-    done
+    # for f in stimuli/Group$i/shape/*
+    # do
+    #     # ffmpeg -hide_banner  -y -i $f -filter_complex 'tpad=stop=1' stimuli/Group$i/material/$(basename $f .mp4)_fixed.mp4
+    #     mv stimuli/Group$i/material/$(basename $f .mp4)_fixed.mp4 stimuli/Group$i/material/$(basename $f .mp4).mp4
+    # done
+# done
+
+for f in instructions/*.mp4
+do
+    ffmpeg -hide_banner  -y -i $f -filter_complex 'tpad=stop=1' instructions/$(basename $f .mp4)_fixed.mp4
+    mv instructions/$(basename $f .mp4)_fixed.mp4 instructions/$(basename $f .mp4).mp4
 done
