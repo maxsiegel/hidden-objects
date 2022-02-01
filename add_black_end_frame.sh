b9c8e28856041087b5605c8d6bc692b77198f9cd
Max Siegel <maxs@mit.edu>
2022-02-01 12:54:11 -0500

    ending black frame

---
+++ add_black_end_frame.sh
@@ -0,0 +1,13 @@
+for i in $(seq 1 3)
+do
+    # for f in stimuli/Group$i/material/*
+    # do
+    #     ffmpeg -hide_banner  -y -i $f -filter_complex 'tpad=stop=1' stimuli/Group$i/shape/$(basename $f .mp4)_fixed.mp4
+    #     mv stimuli/Group$i/shape/$(basename $f .mp4)_fixed.mp4 stimuli/Group$i/shape/$(basename $f .mp4).mp4
+    # done
+    for f in stimuli/Group$i/shape/*
+    do
+        # ffmpeg -hide_banner  -y -i $f -filter_complex 'tpad=stop=1' stimuli/Group$i/material/$(basename $f .mp4)_fixed.mp4
+        mv stimuli/Group$i/material/$(basename $f .mp4)_fixed.mp4 stimuli/Group$i/material/$(basename $f .mp4).mp4
+    done
+done
