a418c25b1c231038d4ea1253d3ede332e49ef677
Max Siegel <maxs@mit.edu>
2022-01-31 21:54:58 -0500

    data compositing again

--- composite_videos.py
+++ composite_videos.py
@@ -1,4 +1,5 @@
 import os
+from numpy.random import binomial
 import re
 from shutil import copy
 import subprocess as sp
@@ -32,21 +33,29 @@ for group in ['Group' + str(i) for i in range(1, 4)]:
 
     material_trials = group_trials['matStim']
 
-    # for trial in extract_strings(material_trials):
-    #     stim_name = trial[0] + '_' + trial[1] + '.mp4'
+    for trial in extract_strings(material_trials):
 
-    #     # args 2 3 4 are stimuli filenames
-    #     cmd = [
-    #         'bash',
-    #         'composite_videos.sh',
-    #         join(group_base_path, 'material', stim_name),
-    #         trial[2],
-    #         trial[3],
-    #         trial[4],
-    #     ]
+        if binomial(1, .5):
+            trial[3] = trial[3].replace('png', 'mp4')
+            trial[4] = trial[4].replace('png', 'mp4')
+        else:
+            trial[3] = trial[4].replace('png', 'mp4')
+            trial[4] = trial[3].replace('png', 'mp4')
 
-    #     sp.call(cmd)
-    #     paths[group]['material'].append(join(group_base_path, 'material', stim_name))
+        # stim_name = trial[0] + '_' + trial[1] + '.mp4'
+        stim_name = trial[2].split('.')[0] + '__' + trial[3].split('.')[0] + '__' + trial[4].split('.')[0] + '.mp4'
+        # args 2 3 4 are stimuli filenames
+        cmd = [
+            'bash',
+            'composite_videos.sh',
+            join(group_base_path, 'material', stim_name),
+            trial[2],
+            trial[3],
+            trial[4],
+        ]
+
+        sp.call(cmd)
+        paths[group]['material'].append(join(group_base_path, 'material', stim_name))
 
 
     paths[group]['shape'] = []
@@ -54,21 +63,26 @@ for group in ['Group' + str(i) for i in range(1, 4)]:
 
     for trial in extract_strings(shape_trials):
         # print(trial)
+        # import pdb; pdb.set_trace()
 
         a_or_b = re.findall('[A-B][^A-B]*', trial[3])[1][0]
 
-        folder = trial[0] + '_' + trial[1] + a_or_b
+        # folder = trial[0] + '_' + trial[1] + a_or_b
         # folder = "_".join(trial)
-        stim_name = folder + '.mp4'
         # os.makedirs(join(group_base_path, 'shape', folder), exist_ok=True)
         # copy(join('videos', trial[2]), join(group_base_path, 'shape', folder, stim_name))
         # copy(join('videos', trial[3]), join(group_base_path, 'shape', folder, trial[3]))
         # copy(join('videos', trial[4]), join(group_base_path, 'shape', folder, trial[4]))
 
         trial[2] = trial[2].replace('png', 'mp4')
-        trial[3] = trial[3].replace('png', 'mp4')
-        trial[4] = trial[4].replace('png', 'mp4')
+        if binomial(1, .5):
+            trial[3] = trial[3].replace('png', 'mp4')
+            trial[4] = trial[4].replace('png', 'mp4')
+        else:
+            trial[3] = trial[4].replace('png', 'mp4')
+            trial[4] = trial[3].replace('png', 'mp4')
 
+        stim_name =  trial[2].split('.')[0] + '__' + trial[3].split('.')[0] + '__' + trial[4].split('.')[0] + '.mp4'
         cmd = [
             'bash',
             'composite_videos.sh',
@@ -77,10 +91,10 @@ for group in ['Group' + str(i) for i in range(1, 4)]:
             trial[3],
             trial[4],
         ]
-        print('command: ' + ' '.join(cmd))
+        # print('command: ' + ' '.join(cmd))
         sp.call(cmd)
         paths[group]['shape'].append(# [
-            join(group_base_path, 'shape', folder, stim_name)# ,
+            join(group_base_path, 'shape', stim_name)# ,
                                       # join(group_base_path, 'shape', folder, trial[3]),
                                       # join(group_base_path, 'shape', folder, trial[4])# ]
         )
